f9b84ac5509e6aa4a0b74607338f2af6f9544cf1
HCL <429068173@qq.com>
2019-11-11 23:30:18 +0800

    ver1.2 上线，实现代码更新后使用Fabric自动部署

--- Djangoblog/fabfile.py
+++ Djangoblog/fabfile.py
@@ -1,6 +1,13 @@
-from fabric import task
+from fabric.api import env, cd, run
 from invoke import Responder
-from ._credentials import github_username, github_password
+from _credentials import github_username, github_password
+'''这里采用的是fabric3 1.14.post1,在最新版的fabric中不知为什么使用默认用户root登录不成功，原因未知。
+   
+   在fabric3中的run函数并无watchers传参。'''
+env.hosts = ['112.124.6.127', ]
+env.user = 'huang'
+env.port = 22
+env.password = 'a2251029'
 
 
 def _get_github_auth_responders():
@@ -18,31 +25,30 @@ def _get_github_auth_responders():
     return [username_responder, password_responder]
 
 
-@task()
-def deploy(c):
+def deploy():
     supervisor_conf_path = '~/etc/'
     supervisor_program_name = 'Djangoblog'
 
     project_root_path = '~/apps/Djangoblog/Djangoblog'
 
     # 先停止应用
-    with c.cd(supervisor_conf_path):
+    with cd(supervisor_conf_path):
         cmd = 'supervisorctl stop {}'.format(supervisor_program_name)
-        c.run(cmd)
+        run(cmd)
 
     # 进入项目根目录，从 Git 拉取最新代码
-    with c.cd(project_root_path):
+    with cd(project_root_path):
         cmd = 'git pull'
         responders = _get_github_auth_responders()
-        c.run(cmd, watchers=responders)
+        run(cmd)
 
     # 安装依赖，迁移数据库，收集静态文件
-    with c.cd(project_root_path):
-        c.run('pipenv install --deploy --ignore-pipfile')
-        c.run('pipenv run python manage.py migrate')
-        c.run('pipenv run python collectstatic --noinput')
+    with cd(project_root_path):
+        run('pipenv install --deploy --ignore-pipfile')
+        run('pipenv run python manage.py migrate')
+        run('pipenv run python manage.py collectstatic --noinput')
 
     # 重新启动应用
-    with c.cd(supervisor_conf_path):
+    with cd(supervisor_conf_path):
         cmd = 'supervisorctl start {}'.format(supervisor_program_name)
-        c.run(cmd)
+        run(cmd)
